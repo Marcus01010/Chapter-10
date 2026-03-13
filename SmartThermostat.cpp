0951f8e1991dd9174f3f001c9712721366ea6ea4
Marcus Lewis <mlewis43@aum.edu>
2026-03-13 17:58:12 -0500

    Add Thermostat class with temperature setting functionality
    Implement a simple thermostat class with temperature control.

---
+++ SmartThermostat.cpp
@@ -0,0 +1,46 @@
+#include
+using namespace std;
+
+class Thermostat
+{
+private:
+int temperature;
+
+public:
+Thermostat()
+{
+temperature = 24;
+}
+
+void set_temperature(int t)
+{
+    if (t >= 16 && t <= 30)
+    {
+        temperature = t;
+    }
+    else
+    {
+        cout << "Invalid temperature!" << endl;
+    }
+}
+
+int get_temperature()
+{
+    return temperature;
+}
+};
+
+int main()
+{
+Thermostat room;
+
+int new_temp;
+cin >> new_temp;
+
+room.set_temperature(new_temp);
+
+cout << "Current temperature: " << room.get_temperature() << endl;
+
+return 0;
+}
+
