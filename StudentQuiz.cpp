0bc5dd724e682cfe8e278bf2f0f78e7846dea508
Marcus Lewis <mlewis43@aum.edu>
2026-03-13 17:56:47 -0500

    Implement QuizResult class for score management

---
+++ StudentQuiz.cpp
@@ -0,0 +1,40 @@
+#include
+using namespace std;
+
+class QuizResult
+{
+private:
+int score;
+
+public:
+void set_score(int new_score)
+{
+if (new_score >= 0 && new_score <= 100)
+{
+score = new_score;
+}
+else
+{
+cout << "Invalid score!" << endl;
+}
+}
+
+int get_score()
+{
+    return score;
+}
+};
+
+int main()
+{
+QuizResult quiz;
+
+int input_score;
+cin >> input_score;
+
+quiz.set_score(input_score);
+
+cout << "Final score: " << quiz.get_score() << endl;
+
+return 0;
+}
